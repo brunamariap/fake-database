b49be03a7c3dddc4546d7dd68bd781b90a4cba52
Bruna Maria <bruna.mariap@outlook.com>
2023-05-20 17:06:19 -0300

    Criar estudante

--- gerador.py
+++ gerador.py
@@ -2,59 +2,38 @@ import random
 from faker import Faker
 import psycopg2
 
-fake = Faker()
+fake = Faker('pt-BR')
 
 # Conexão com o banco de dados
 conn = psycopg2.connect(
     dbname="bancofake",
-    user="postgresfake",
-    password="12345",
+    user="postgres",
+    password="admin",
     host="localhost",
     port="5432"
 )
 cursor = conn.cursor()
 
 # Configurar a quantidade de linhas que você deseja gerar para cada tabela
-qtd_teachers = 200000
-qtd_students = 500
+qtd_teachers = 20000
+qtd_students = 50000
 qtd_courses = 50000
 qtd_schedules = 500000
 qtd_disciplines = 1000
+qtd_classes = 5000
 
-# Gerar e inserir dados na tabela Cidade
-for _ in range(qtd_cidades):
-    cursor.execute("INSERT INTO Cidade (nome) VALUES (%s)", (fake.city(),))
 
-# Gerar e inserir dados na tabela Universidade
-for _ in range(qtd_universidades):
-    cidade_id = random.randint(1, qtd_cidades)
-    cursor.execute("INSERT INTO Universidade (nome, cidade_id) VALUES (%s, %s)", (fake.company(), cidade_id))
+# Gerar e inserir dados na tabela Teacher
+for i in range(qtd_teachers):
+    cursor.execute('INSERT INTO teacher (id, registration, name, profile_photo, departament) VALUES (%s, %s, %s, %s, %s)', (i+1, i+1, fake.name(), fake.file_path(depth=3), 'Professor'))
 
-# Gerar e inserir dados na tabela Professor
-for _ in range(qtd_professores):
-    cidade_id = random.randint(1, qtd_cidades)
-    cursor.execute(
-        "INSERT INTO Professor (nome, data_nascimento, cidade_id) VALUES (%s, %s, %s)",
-        (fake.name(), fake.date_of_birth(tzinfo=None, minimum_age=25, maximum_age=65), cidade_id)
-    )
+# Gerar e inserir dados na tabela Student
+for x in range(qtd_students):
+    cursor.execute('INSERT INTO student (id, registration, name, profile_photo, class_id) VALUES (%s, %s, %s, %s, %s)', (x+1, f'20211094{x}', fake.name(), fake.file_path(depth=3), random.randint(1, qtd_classes)))
 
-# Gerar e inserir dados na tabela Aluno
-for _ in range(qtd_alunos):
-    cidade_id = random.randint(1, qtd_cidades)
-    universidade_id = random.randint(1, qtd_universidades)
-    cursor.execute(
-        "INSERT INTO Aluno (nome, data_nascimento, cidade_id, universidade_id) VALUES (%s, %s, %s, %s)",
-        (fake.name(), fake.date_of_birth(tzinfo=None, minimum_age=18, maximum_age=30), cidade_id, universidade_id)
-    )
-
-# Gerar e inserir dados na tabela Disciplina
-for _ in range(qtd_disciplinas):
-    professor_id = random.randint(1, qtd_professores)
-    universidade_id = random.randint(1, qtd_universidades)
-    cursor.execute(
-        "INSERT INTO Disciplina (nome, professor_id, universidade_id) VALUES (%s, %s, %s)",
-        (fake.job(), professor_id, universidade_id)
-    )
+# Gerar e inserir dados na tabela Schedule
+for x in range(qtd_schedules):
+    cursor.execute('INSERT INTO schedule (id, discipline_id, quantity, weekday, start_time, end_time, class_id) VALUES (%s, %s, %s, %s, %s, %s, %s)', ())
 
 # Confirmar as alterações e fechar a conexão
 conn.commit()
