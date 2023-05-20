cf8b081f12bdf115a3f0a2906dfd225be0f51e73
Felippe <felipperian365@gmail.com>
2023-05-20 16:44:36 -0300

    Criar turmas, cursos e disciplinas

--- gerador.py
+++ gerador.py
@@ -2,13 +2,13 @@ import random
 from faker import Faker
 import psycopg2
 
-fake = Faker()
+fake = Faker('pt-BR')
 
 # Conexão com o banco de dados
 conn = psycopg2.connect(
-    dbname="bancofake",
-    user="postgresfake",
-    password="12345",
+    dbname="fake-database",
+    user="postgres",
+    password="admin",
     host="localhost",
     port="5432"
 )
@@ -17,44 +17,36 @@ cursor = conn.cursor()
 # Configurar a quantidade de linhas que você deseja gerar para cada tabela
 qtd_teachers = 200000
 qtd_students = 500
-qtd_courses = 50000
+qtd_courses = 50000 
+qtd_classes = 5000 
 qtd_schedules = 500000
 qtd_disciplines = 1000
 
-# Gerar e inserir dados na tabela Cidade
-for _ in range(qtd_cidades):
-    cursor.execute("INSERT INTO Cidade (nome) VALUES (%s)", (fake.city(),))
+cursor.execute('DELETE FROM Course')
+cursor.execute('DELETE FROM Discipline')
+cursor.execute('DELETE FROM Teacher')
 
-# Gerar e inserir dados na tabela Universidade
-for _ in range(qtd_universidades):
-    cidade_id = random.randint(1, qtd_cidades)
-    cursor.execute("INSERT INTO Universidade (nome, cidade_id) VALUES (%s, %s)", (fake.company(), cidade_id))
+for x in range(qtd_courses):
+    cursor.execute("INSERT INTO Course (id, name, degree, course_load, byname) VALUES (%s, %s, %s, %s, %s)", (x, fake.catch_phrase(), random.randint(1, 7), random.randint(1000, 3500), fake.catch_phrase()))
 
-# Gerar e inserir dados na tabela Professor
-for _ in range(qtd_professores):
-    cidade_id = random.randint(1, qtd_cidades)
-    cursor.execute(
-        "INSERT INTO Professor (nome, data_nascimento, cidade_id) VALUES (%s, %s, %s)",
-        (fake.name(), fake.date_of_birth(tzinfo=None, minimum_age=25, maximum_age=65), cidade_id)
-    )
+for x in range(qtd_disciplines):
+    workloads = [
+        30,
+        60,
+        80,
+        120,
+    ]
+    worload_random = workloads[random.randint(0, 3)]
+    cursor.execute("INSERT INTO Discipline (id, name, code, workload_in_clock, workload_in_class, is_optional) VALUES (%s, %s, %s, %s, %s, %s)", (x, fake.job(), f"TEC-{x}", (worload_random * 45) / 60, worload_random, bool(random.randint(0, 1))))
 
-# Gerar e inserir dados na tabela Aluno
-for _ in range(qtd_alunos):
-    cidade_id = random.randint(1, qtd_cidades)
-    universidade_id = random.randint(1, qtd_universidades)
-    cursor.execute(
-        "INSERT INTO Aluno (nome, data_nascimento, cidade_id, universidade_id) VALUES (%s, %s, %s, %s)",
-        (fake.name(), fake.date_of_birth(tzinfo=None, minimum_age=18, maximum_age=30), cidade_id, universidade_id)
-    )
-
-# Gerar e inserir dados na tabela Disciplina
-for _ in range(qtd_disciplinas):
-    professor_id = random.randint(1, qtd_professores)
-    universidade_id = random.randint(1, qtd_universidades)
-    cursor.execute(
-        "INSERT INTO Disciplina (nome, professor_id, universidade_id) VALUES (%s, %s, %s)",
-        (fake.job(), professor_id, universidade_id)
-    )
+# for x in range(qtd_classes):
+#     shifts = [
+#         'Matutino',
+#         'Vespertino',
+#         'Noturno'
+#     ]
+#     shift_random = shifts[random.randint(0,2)]
+#     cursor.execute("INSERT INTO Class (id, class_leader, course_id, reference_period, shift) VALUES (%s, %s, %s, %s, %s)", (x, x, x, random.randint(1, 7), shift_random))
 
 # Confirmar as alterações e fechar a conexão
 conn.commit()
